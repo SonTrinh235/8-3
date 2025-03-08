a2608b1eb55feac25b7c45c92ccb6c05d8d9a13a
Trịnh Đức Sơn <ducsondz2005@gmail.com>
2025-03-08 09:42:30 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const message = [
+    "abc"
+]
+function handleRightClick() {
+    window.location.href = "Right.html";
+}
+
+let idx =1;
+
+function handleLeftClick() {
+    window.location.href = "Wrong.html";
+}
+
+function handleReturn(){
+    window.location.href = "index.html";
+}
